bde4a1be0d19b91a531f6c4527c1d2918f5c0392
Alberto <albertoscuola02@gmail.com>
2024-04-08 00:23:28 +0200

    add all fit for braggkleeman

---
+++ spettroscopia/bragg/bragg_dati_v1/root_analisi_1/fit_width/macro/MacroGraphRange_cc.d
@@ -0,0 +1,12 @@
+
+# DO NOT DELETE
+
+./MacroGraphRange_cc.so: /usr/include/root/TGraphErrors.h
+./MacroGraphRange_cc.so: /usr/include/root/TAxis.h
+./MacroGraphRange_cc.so: /usr/include/root/TCanvas.h
+./MacroGraphRange_cc.so: /usr/include/root/TMultiGraph.h
+./MacroGraphRange_cc.so: /usr/include/root/TF1.h
+./MacroGraphRange_cc.so: /usr/include/root/TFitResult.h
+./MacroGraphRange_cc.so: /usr/include/root/TFitResultPtr.h
+./MacroGraphRange_cc.so: /usr/include/root/RVersion.h /usr/include/root/ROOT/RConfig.hxx /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
+MacroGraphRange_cc__ROOTBUILDVERSION= 6.30/04
