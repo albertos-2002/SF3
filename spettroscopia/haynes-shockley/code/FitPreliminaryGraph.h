b45528021d99f5f38e5ad2dc4f276fbe62d639fc
Alberto <93335022+albertos-2002@users.noreply.github.com>
2024-06-24 16:27:37 +0200

    Update FitPreliminaryGraph.h

--- spettroscopia/haynes-shockley/code/FitPreliminaryGraph.h
+++ spettroscopia/haynes-shockley/code/FitPreliminaryGraph.h
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <map>
 
+#include <TApplication.h>
+#include <TGraphErrors.h>
+#include <TMultiGraph.h>
+#include <TLegend.h>
+#include <TFile.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TPad.h>
+#include <TLine.h>
+#include <TAxis.h>
+#include <TSystem.h>
+
+#include "ExternalObjects.h"
+using namespace std;
 
 
 
